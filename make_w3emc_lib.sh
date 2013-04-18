968021da40b0b62249e3fb38704ce47568af3381
paul.vandelst@noaa.gov <paul.vandelst@noaa.gov>
2013-04-18 20:33:05 +0000

    w3emc trunk. Trac Ticket #50
      * configure.ac: Updated version number to v2.1.0. Added checks for
        the ar and ranlib tools. Replaced SUFFIX variable with PRECISION.
        Corrected gfortran precision promotion flags. Added preprocessor
        flags for XL AIX compilers to handle "start" procedure issues
        corrected in r27775.
      
      * Makefile.in: Removed all unused substitution variables. Added
        clean dependency to distclean target.
    
      * make_w3emc_lib.sh: Altered setup file detection to use the entire
        filename so it's less hassle if users wish to add additional setup
        files. Performing the NCO-style installation before the Gnu-style
        one to prevent the existing directory FORCE check from triggering
        an error. Removed copy of config log to the install directory as 
        that is now handled by the install targets. Added cleanup at end 
        of each build cycle.
    
    
      config-setup/
    
      * gfortran-test.setup: New. Setup file used for local testing that
        uses envars to point to the sigio library - via a Gnu-style install.
    
    
      libsrc/
    
      * Makefile.in: Removed all unused package-related substitution variables.
        Replaced use of SUFFIX with PRECISION substitution variable. Added
        required tool-related substitution variables, and deleted unused ones.
        Altered library target to prevent unnecessary rebuilds. Updated all
        install targets. Added FORCE dependency to the nco_install target to 
        prevent installation into exisiting directories. Added suffix rule for
        preprocessing Fortran source.
    
      * make.filelist: Updated list of preprocessed and "regular" Fortran source
        files.
    
      * make.dependencies: Regenerated.

--- make_w3emc_lib.sh
+++ make_w3emc_lib.sh
@@ -19,15 +19,15 @@ if [ $# -ne 1 ]; then
   echo "$0: Must specify build setup. Valid setups:" >&2
   echo "Usage: `basename $0` setup-file" >&2
   echo "  Valid setups:" >&2
-  for FILE in `ls ./config-setup/*.setup`; do
-    echo "    `basename ${FILE} .setup`" >&2
+  for FILE in `ls ./config-setup/`; do
+    echo "    `basename ${FILE}`" >&2
   done
   exit 1
 fi
 
 
 # Source the build setup
-SETUP_FILE="./config-setup/$1.setup"
+SETUP_FILE="./config-setup/$1"
 if [ ! -f ${SETUP_FILE} ]; then
   echo "$0: Cannot find specified setup file ${SETUP_FILE}" >&2
   exit 1
@@ -69,20 +69,20 @@ for PRECISION in ${PRECISION_LIST}; do
   # Install the current build...
   echo; echo
   echo "==============================================================="
-  echo "Performing GNU-type install of precision ${PRECISION} build"
-  echo "==============================================================="
-  echo
-  make install
-
-  echo; echo
-  echo "==============================================================="
   echo "Performing NCO-type install of precision ${PRECISION} build"
   echo "==============================================================="
   echo
   make nco_install
 
-  # Save the build log file.
-  cp config.log config.log_${PRECISION}
+  echo; echo
+  echo "==============================================================="
+  echo "Performing GNU-type install of precision ${PRECISION} build"
+  echo "==============================================================="
+  echo
+  make install
+
+  # Clean up
+  make distclean
 
 done
 
