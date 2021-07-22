9d0efb05577fbb13796a32c25e5144d18266c886
Brian Curtis <brian.curtis@noaa.gov>
2021-07-22 13:54:09 -0400

    Doxygen updates for gbyte.f, part of #77

--- src/gbyte.f
+++ src/gbyte.f
@@ -1,36 +1,34 @@
 C> @file
-C> THIS IS THE FORTRAN VERSION OF GBYTE      
-C      
-C> THIS IS THE FORTRAN VERSION OF GBYTE
-C> @author DR. ROBERT C. GAMMILL, CONSULTANT, NATIONAL CENTER FOR ATMOSPHERIC RESEARCH
-C> @date MAY 1972
+C> @brief This is the fortran version of gbyte.
+C> @author Dr. Robert C. Gammill @date 1972-05-DD
+
+C> This is the fortran version of gbyte
 C>
-C> CHANGES FOR SiliconGraphics IRIS-4D/25
-C> SiliconGraphics 3.3 FORTRAN 77
-C> March 1991, RUSSELL E. JONES
-C> NATIONAL WEATHER SERVICE
+C> Program history log:
+C> - Russell E. Jones 1991-03-DD
+C> Changes for SiliconGraphics IRIS-4D/25
+C> SiliconGraphics 3.3 FORTRAN 77.
 C>
-C> SUBROUTINE GBYTE (IPACKD,IUNPKD,NOFF,NBITS)
+C> To unpack a byte into a target word.  the
+C> unpacked byte is right-justified in the
+C> target word, and the remainder of the
+C> word is zero-filled.
 C>
-C> TO UNPACK A BYTE INTO A TARGET WORD.  THE
-C> UNPACKED BYTE IS RIGHT-JUSTIFIED IN THE
-C> TARGET WORD, AND THE REMAINDER OF THE
-C> WORD IS ZERO-FILLED.
+C> @param[in] IPACKD The word or array containing the byte to be
+C>                   unpacked.
 C>
-C> @param[in] IPACKD THE WORD OR ARRAY CONTAINING THE BYTE TO BE
-C>                   UNPACKED.
+C> @param[out] IUNPKD The word which will contain the unpacked
+C>                   byte.
 C>
-C> @param[out] IUNPKD THE WORD WHICH WILL CONTAIN THE UNPACKED
-C>                   BYTE.
+C> @param[in] NOFF The number of bits to skip, left to right,
+C>                 in IPACKD in order to locate the byte
+C>                 to be unpacked.
 C>
-C> @param[in] NOFF THE NUMBER OF BITS TO SKIP, LEFT TO RIGHT,
-C>                 IN 'IPACKD' IN ORDER TO LOCATE THE BYTE
-C>                 TO BE UNPACKED.
+C> @param[in] NBITS Number of bits in the byte to be unpacked.
+C>                  Maximum of 64 bits on 64 bit machine, 32
+C>                  bits on 32 bit machine.
 C>
-C> @param[in] NBITS NUMBER OF BITS IN THE BYTE TO BE UNPACKED.
-C>                  MAXIMUM OF 64 BITS ON 64 BIT MACHINE, 32
-C>                  BITS ON 32 BIT MACHINE.
-C>
+C> @author Dr. Robert C. Gammill @date 1972-05-DD
       SUBROUTINE GBYTE(IPACKD,IUNPKD,NOFF,NBITS)
       INTEGER    IPACKD(*)
       INTEGER    IUNPKD
@@ -51,14 +49,14 @@ C
          IFIRST = 0
       ENDIF
 C
-C NBITS MUST BE LESS THAN OR EQUAL TO NBITSW                                   
+C NBITS MUST BE LESS THAN OR EQUAL TO NBITSW
 C
       ICON   = NBITSW - NBITS
       IF (ICON.LT.0) RETURN
       MASK   = MASKS(NBITS)
 C
 C INDEX TELLS HOW MANY WORDS INTO THE ARRAY 'IPACKD' THE NEXT BYTE
-C APPEARS.         
+C APPEARS.
 C
       INDEX  = ISHFT(NOFF,JSHIFT)
 C
@@ -66,28 +64,28 @@ C II TELLS HOW MANY BITS THE BYTE IS FROM THE LEFT SIDE OF THE WORD.
 C
       II     = MOD(NOFF,NBITSW)
 C
-C MOVER SPECIFIES HOW FAR TO THE RIGHT NBITS MUST BE MOVED IN ORDER            
+C MOVER SPECIFIES HOW FAR TO THE RIGHT NBITS MUST BE MOVED IN ORDER
 C
-C    TO BE RIGHT ADJUSTED.                                                 
+C    TO BE RIGHT ADJUSTED.
 C
       MOVER = ICON - II
 C
       IF (MOVER.GT.0) THEN
         IUNPKD  = IAND(ISHFT(IPACKD(INDEX+1),-MOVER),MASK)
 C
-C THE BYTE IS SPLIT ACROSS A WORD BREAK.                 
+C THE BYTE IS SPLIT ACROSS A WORD BREAK.
 C
-      ELSE IF (MOVER.LT.0) THEN                      
-        MOVEL = - MOVER                                                       
-        MOVER = NBITSW - MOVEL                                                
+      ELSE IF (MOVER.LT.0) THEN
+        MOVEL = - MOVER
+        MOVER = NBITSW - MOVEL
         IUNPKD  = IAND(IOR(ISHFT(IPACKD(INDEX+1),MOVEL),
      &          ISHFT(IPACKD(INDEX+2),-MOVER)),MASK)
 C
 C THE BYTE IS ALREADY RIGHT ADJUSTED.
-C  
-      ELSE                              
-        IUNPKD  = IAND(IPACKD(INDEX+1),MASK) 
+C
+      ELSE
+        IUNPKD  = IAND(IPACKD(INDEX+1),MASK)
       ENDIF
-C   
+C
       RETURN
       END
