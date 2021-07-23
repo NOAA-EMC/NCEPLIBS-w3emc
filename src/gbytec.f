6127ce43c21130e3ee0935d9920d149d4bf5d469
Brian Curtis <brian.curtis@noaa.gov>
2021-07-23 11:02:26 -0400

    Add the remaining gbyte files plus getbit

--- src/gbytec.f
+++ src/gbytec.f
@@ -1,4 +1,14 @@
 C> @file
+C> @brief Wrapper for gbytesc() limiting NSKIP and N to 0 and 1
+C> @author Unknown
+
+C> Wrapper for gbytesc() limiting NSKIP and N to 0 and 1
+C>
+C> @param[in] IN Character*1 array input.
+C> @param[out] IOUT Unpacked array output.
+C> @param[in] ISKIP Initial number of bits to skip.
+C> @param[in] NBYTE Number of bits to take.
+C>
 
       SUBROUTINE GBYTEC(IN,IOUT,ISKIP,NBYTE)
       character*1 in(*)
