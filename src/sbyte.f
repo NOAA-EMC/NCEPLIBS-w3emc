facf6822a5b940ddd2d4ae35df444c206ee0f334
Brian Curtis <64433609+BrianCurtis-NOAA@users.noreply.github.com>
2021-08-19 11:44:43 -0400

    Doxygen for q9ie32.f r63w72.f sbyte.f sbytec.f sbytes.f (Part of #77) (#102)
    Doxygen additions for src/q9ie32.f src/r63w72.f src/sbyte.f src/sbytec.f src/sbytes.f

--- src/sbyte.f
+++ src/sbyte.f
@@ -1,16 +1,14 @@
 C> @file
-C> @brief THIS IS THE FORTRAN 32 bit VERSION OF SBYTE.
-C      
-C> @author DR. ROBERT C. GAMMILL, CONSULTANT, NATIONAL CENTER FOR ATMOSPHERIC RESEARCH
-C> @date JULY 1972 
+C> @brief This is the fortran 32 bit version of sbyte().
+C> @author Robert Gammill @date 1972-07
+
+C> @param[out] IOUT
+C> @param[in] IN Unpacked array input
+C> @param[in] ISKIP Initial number of bits to skip
+C> @param[in] NBYTE Number of bits to pack
 C>
-C> THIS IS THE FORTRAN 32 bit VERSION OF SBYTE.
-C> Changes for SiliconGraphics IRIS-4D/25
-C> SiliconGraphics 3.3 FORTRAN 77
-C> MARCH 1991  RUSSELL E. JONES
-C> NATIONAL WEATHER SERVICE
-C>
-      SUBROUTINE SBYTE(IOUT,IN,ISKIP,NBYTE)                            
+C> @author Robert Gammill @date 1972-07
+      SUBROUTINE SBYTE(IOUT,IN,ISKIP,NBYTE)
       INTEGER    IN
       INTEGER    IOUT(*)
       INTEGER    MASKS(32)
@@ -28,49 +26,49 @@ C    &             Z'001FFFFF',Z'003FFFFF',Z'007FFFFF',Z'00FFFFFF',
 C    &             Z'01FFFFFF',Z'03FFFFFF',Z'07FFFFFF',Z'0FFFFFFF',
 C    &             Z'1FFFFFFF',Z'3FFFFFFF',Z'7FFFFFFF',Z'FFFFFFFF'/
 C
-C     MASK TABLE PUT IN DECIMAL SO IT WILL COMPILE ON AN 32 BIT 
-C     COMPUTER 
+C     MASK TABLE PUT IN DECIMAL SO IT WILL COMPILE ON AN 32 BIT
+C     COMPUTER
 C
       DATA  MASKS / 1, 3, 7, 15, 31, 63, 127, 255, 511, 1023, 2047,
      & 4095, 8191, 16383, 32767, 65535, 131071, 262143, 524287,
      & 1048575, 2097151, 4194303, 8388607, 16777215, 33554431,
      & 67108863, 134217727, 268435455, 536870911, 1073741823,
      & 2147483647, -1/
-C                                                                               
+C
 C NBYTE MUST BE LESS THAN OR EQUAL TO NBITSW
 C
         ICON  = NBITSW - NBYTE
-        IF (ICON.LT.0) RETURN   
+        IF (ICON.LT.0) RETURN
         MASK  = MASKS(NBYTE)
 C
-C INDEX TELLS HOW MANY WORDS INTO IOUT THE NEXT BYTE IS TO BE STORED.           
+C INDEX TELLS HOW MANY WORDS INTO IOUT THE NEXT BYTE IS TO BE STORED.
 C
-        INDEX = ISHFT(ISKIP,-5) 
+        INDEX = ISHFT(ISKIP,-5)
 C
-C II TELLS HOW MANY BITS IN FROM THE LEFT SIDE OF THE WORD TO STORE IT.         
+C II TELLS HOW MANY BITS IN FROM THE LEFT SIDE OF THE WORD TO STORE IT.
 C
         II    = MOD(ISKIP,NBITSW)
 C
-        J     = IAND(MASK,IN)    
-        MOVEL = ICON - II         
-C                                                                               
-C BYTE IS TO BE STORED IN MIDDLE OF WORD.  SHIFT LEFT.                          
+        J     = IAND(MASK,IN)
+        MOVEL = ICON - II
 C
-        IF (MOVEL.GT.0) THEN          
-          MSK           = ISHFT(MASK,MOVEL)  
+C BYTE IS TO BE STORED IN MIDDLE OF WORD.  SHIFT LEFT.
+C
+        IF (MOVEL.GT.0) THEN
+          MSK           = ISHFT(MASK,MOVEL)
           IOUT(INDEX+1) = IOR(IAND(NOT(MSK),IOUT(INDEX+1)),
      &    ISHFT(J,MOVEL))
-C                                                                               
-C THE BYTE IS TO BE SPLIT ACROSS A WORD BREAK.                                  
 C
-        ELSE IF (MOVEL.LT.0) THEN          
-          MSK           = MASKS(NBYTE+MOVEL)      
+C THE BYTE IS TO BE SPLIT ACROSS A WORD BREAK.
+C
+        ELSE IF (MOVEL.LT.0) THEN
+          MSK           = MASKS(NBYTE+MOVEL)
           IOUT(INDEX+1) = IOR(IAND(NOT(MSK),IOUT(INDEX+1)),
-     &    ISHFT(J,MOVEL))  
+     &    ISHFT(J,MOVEL))
           ITEMP         = IAND(MASKS(NBITSW+MOVEL),IOUT(INDEX+2))
           IOUT(INDEX+2) = IOR(ITEMP,ISHFT(J,NBITSW+MOVEL))
-C             
-C BYTE IS TO BE STORED RIGHT-ADJUSTED.                                          
+C
+C BYTE IS TO BE STORED RIGHT-ADJUSTED.
 C
         ELSE
           IOUT(INDEX+1) = IOR(IAND(NOT(MASK),IOUT(INDEX+1)),J)
--- src/sbytec.f
+++ src/sbytec.f
@@ -1,4 +1,14 @@
 C> @file
+C> @brief Wrapper for sbytesc()
+C> @author Unknown
+
+C> This is a wrapper for sbytesc()
+C> @param[in] OUT = packed array output
+C> @param[in] IN = unpacked array input
+C> @param[in] ISKIP = initial number of bits to skip
+C> @param[in] NBYTE = number of bits to pack
+C>
+C> @author Unknown
 
       SUBROUTINE SBYTEC(OUT,IN,ISKIP,NBYTE)
       character*1 out(*)
