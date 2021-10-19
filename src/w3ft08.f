3a25a86385c99f4b26e459d0937a53e5915f8027
Brian Curtis <Brian.Curtis@noaa.gov>
2021-10-19 11:07:22 -0400

    Doxygen for src/w3ft06.f src/w3ft06v.f src/w3ft07.f src/w3ft08.f src/w3ft09.f

--- src/w3ft08.f
+++ src/w3ft08.f
@@ -1,66 +1,48 @@
 C> @file
-C
-C> SUBPROGRAM: W3FT08         COMPUTES 2.5 X 2.5 N. HEMI. GRID-SCALER
-C>   AUTHOR: SELA,JOE         ORG: W323       DATE: 80-11-21
+C> @brief Computes 2.5 x 2.5 n. hemi. grid-scaler.
+C> @author Joe Sela @date 1988-06-20
+
+C> Computes 2.5 x 2.5 n. hemi. grid of 145 x 37 points
+C> from spectral coefficients in a rhomboidal 30 resolution
+C> representing a scaler field.
 C>
-C> ABSTRACT: COMPUTES 2.5 X 2.5 N. HEMI. GRID OF 145 X 37 POINTS 
-C>   FROM SPECTRAL COEFFICIENTS IN A RHOMBOIDAL 30 RESOLUTION 
-C>   REPRESENTING A SCALER FIELD.
+C> ### Program History Log:
+C> Date | Programmer | Comment
+C> -----|------------|--------
+C> 1988-06-20 | Joe Sela | Initial.
+C> 1988-06-20 | Ralph Jones | Change to microsoft fortran 4.10.
+C> 1990-06-12 | Ralph Jones | Change to sun fortran 1.3.
+C> 1991-03-30 | Ralph Jones | Convert to silicongraphics fortran.
+C> 1993-03-29 | Ralph Jones | Add save statement.
+C> 1993-07-22 | Ralph Jones | Change double precision to real for cray.
 C>
-C> PROGRAM HISTORY LOG:
+C> @param[in] FLN 961 complex coeff.
+C> @param[in] PLN 992 real space for legendre polynomials.
+C> @param[in] EPS 992 real space for
+C> coeffs. used in computing pln.
+C> @param[in] FL 31 complex space for fourier coeff.
+C> @param[in] WORK 144 real work space for subr. w3ft12()
+C> @param[in] TRIGS 216 precomputed trig funcs. used
+C> in w3ft12(), computed by w3fa13()
+C> @param[out] GN (145,37) grid values. 5365 point grid is type 29 or
+C> 1d hex o.n. 84
 C>
-C>   88-06-20  JOE SELA
-C>   88-06-20  R.E.JONES   CHANGE TO MICROSOFT FORTRAN 4.10
-C>   90-06-12  R.E.JONES   CHANGE TO SUN FORTRAN 1.3
-C>   91-03-30  R.E.JONES   CONVERT TO SiliconGraphics FORTRAN
-C>   93-03-29  R.E.JONES   ADD SAVE STATEMENT
-C>   93-07-22  R.E.JONES   CHANGE DOUBLE PRECISION TO REAL FOR CRAY
+C> @note This subroutine was optimized to run in a small amount of
+C> memory, it is not optimized for speed, 70 percent of the time is
+C> used by subroutine w3fa12 computing the legendre polynomials. since
+C> the legendre polynomials are constant they need to be computed
+C> only once in a program. By moving w3fa12() to the main program and
+C> computing pln as a (32,31,37) array and changing this subroutine
+C> to use pln as a three dimension array you can cut the running time
+C> 70 percent. w3ft38() has these improvements.
 C>
-C> USAGE:  CALL W3FT08(FLN,GN,PLN,EPS,FL,WORK,TRIGS)
-C>
-C>   INPUT VARIABLES:
-C>     NAMES  INTERFACE DESCRIPTION OF VARIABLES AND TYPES
-C>     ------ --------- -----------------------------------------------
-C>     FLN    ARG LIST  961 COMPLEX COEFF.
-C>     PLN    ARG LIST  992 REAL SPACE FOR LEGENDRE POLYNOMIALS.
-C>     EPS    ARG LIST  992 REAL SPACE FOR
-C>                      COEFFS. USED IN COMPUTING PLN.
-C>     FL     ARG LIST  31 COMPLEX SPACE FOR FOURIER COEFF.
-C>     WORK   ARG LIST  144 REAL WORK SPACE FOR SUBR. W3FT12
-C>     TRIGS  ARG LIST  216 PRECOMPUTED TRIG FUNCS. USED
-C>                      IN W3FT12, COMPUTED BY W3FA13
-C>
-C>   OUTPUT VARIABLES:
-C>     NAMES  INTERFACE DESCRIPTION OF VARIABLES AND TYPES
-C>     ------ --------- -----------------------------------------------
-C>     GN     ARG LIST  (145,37) GRID VALUES.
-C>                      5365 POINT GRID IS TYPE 29 OR 1D HEX O.N. 84
-C>
-C>   SUBPROGRAMS CALLED:
-C>     NAMES                                                   LIBRARY
-C>     ------------------------------------------------------- --------
-C>     AIMAG  CMPLX  REAL                                      SYSTEM  
-C>     W3FA12 W3FT12                                           W3LIB
-C>
-C> WARNING: THIS SUBROUTINE WAS OPTIMIZED TO RUN IN A SMALL AMOUNT OF
-C>   MEMORY, IT IS NOT OPTIMIZED FOR SPEED, 70 PERCENT OF THE TIME IS
-C>   USED BY SUBROUTINE W3FA12 COMPUTING THE LEGENDRE POLYNOMIALS. SINCE
-C>   THE LEGENDRE POLYNOMIALS ARE CONSTANT THEY NEED TO BE COMPUTED
-C>   ONLY ONCE IN A PROGRAM. BY MOVING W3FA12 TO THE MAIN PROGRAM AND
-C>   COMPUTING PLN AS A (32,31,37) ARRAY AND CHANGING THIS SUBROUTINE
-C>   TO USE PLN AS A THREE DIMENSION ARRAY YOU CAN CUT THE RUNNING TIME
-C>   70 PERCENT.  W3FT38 HAS THESE IMPROVEMENTS.
-C>
-C> ATTRIBUTES:
-C>   LANGUAGE: CFT77 FORTRAN 77
-C>   MACHINE:  CRAY Y-MP8/864, CRAY Y-MP EL2/128
-C>
+C> @author Joe Sela @date 1988-06-20
       SUBROUTINE W3FT08(FLN,GN,PLN,EPS,FL,WORK,TRIGS)
 C
-       COMPLEX          FL( 31 ) 
-       COMPLEX          FLN( 31 , 31 ) 
+       COMPLEX          FL( 31 )
+       COMPLEX          FLN( 31 , 31 )
 C
-       REAL             COLRA 
+       REAL             COLRA
        REAL             EPS(992)
        REAL             GN(145,37)
        REAL             PLN( 32 , 31 )
@@ -71,18 +53,18 @@ C
 C
        DATA  PI    /3.14159265/
 C
-         DRAD = 2.5 * PI / 180.0 
+         DRAD = 2.5 * PI / 180.0
 C
          DO 400 LAT = 1,37
            LATN  = 38 - LAT
-           COLRA = (LAT - 1) * DRAD 
-           CALL W3FA12(PLN,COLRA, 30 ,EPS) 
+           COLRA = (LAT - 1) * DRAD
+           CALL W3FA12(PLN,COLRA, 30 ,EPS)
 C
              DO 100 L = 1, 31
                FL(L) = (0.,0.)
  100         CONTINUE
 C
-               DO 300 L = 1, 31 
+               DO 300 L = 1, 31
                  DO 200 I = 1, 31
                    FL(L) = FL(L) + CMPLX(PLN(I,L) * REAL(FLN(I,L)) ,
      &             PLN(I,L) * AIMAG(FLN(I,L)) )
