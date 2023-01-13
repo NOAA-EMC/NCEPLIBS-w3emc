f705b8e4aaa803f99cca1c11fa7d03d45ca53661
George.Gayno <george.gayno@noaa.gov>
2023-01-13 13:35:56 +0000

    Update unit test.
    Fixes #157.

--- tests/test_w3fi71.f90
+++ tests/test_w3fi71.f90
@@ -1,5 +1,10 @@
  program test_w3fi71
    implicit none
+   integer :: igrid, igds(18), ierr
    print*,"Testing w3fi71..."
+   igrid = 172
+   call w3fi71(igrid, igds, ierr)
+   print*,'ierr ',ierr
+   print*,'igds ',igds
    print*,"SUCCESS"
  end program test_w3fi71
