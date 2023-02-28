58cec37343f0e5013a1fa927bf8eba1a1a170952
Edward Hartnett <edward.hartnett@noaa.gov>
2023-02-28 12:57:15 -0700

    testing

--- tests/test_w3fi74.F90
+++ tests/test_w3fi74.F90
@@ -5,48 +5,40 @@
 ! Ed Hartnett, 2/28/23
 program test_w3fi74
   implicit none
-  integer :: i
-!  integer :: iret
-  integer :: kf, nbit
-  parameter(kf = 4)
-  parameter(nbit = 8)
-  integer maxbit
-  parameter(maxbit=24)  
-!  character pds(400)
-!  character grib(1000+kf*12)  
-  real f(kf)
-  integer igflag, igrid
-  parameter (igflag = 0)
-  integer ipds(200),igds(200)
+  integer igrid
+  integer igds(200)
   integer icomp
-!  integer kfo, lgrib
-  integer ierr
   integer npts
   character*1 gds(200)
   integer lengds
+  integer ierr
+  integer i
+  character expected_gds(32)
+  expected_gds(:) = (/ char(0), char(0), char(32), char(0), &
+       char(255), char(5), char(2), char(178), char(2), &
+       char(198), char(128), char(144), char(35), char(131), &
+       char(92), char(34), char(0), char(129), char(56), &
+       char(128), char(0), char(49), char(156), char(0), &
+       char(49), char(156), char(128), char(64), char(0), &
+       char(0), char(0), char(0) /)
 
   print *, "Testing w3fi74..."
 
-  ! Fill up some test data.
-  do i = 1, 200
-     ipds(i) = 0
-  end do
-  do i = 1, kf
-     f(i) = i / 2
-  end do
-
   ! Fill the igds array. This call comes from test_w3fi71.F90.
-  icomp = 0
   igrid = 172
   call w3fi71(igrid, igds, ierr)
   if (ierr .ne. 0) stop 1
 
   ! Fill the igds array. This call comes from w3if72.f.
+  icomp = 0
   npts = 4
   call w3fi74(igds, icomp, gds, lengds, npts, ierr)
-!  print *, lengds, npts
   if (ierr .ne. 0) stop 1
   if (lengds .ne. 32 .or. npts .ne. 489900) stop 2
+  do i = 1, 32
+     if (gds(i) .ne. expected_gds(i)) stop 4
+     !print *,'char(', ichar(gds(i)), '), '
+  end do
 
   print *, "SUCCESS"
 end program test_w3fi74
