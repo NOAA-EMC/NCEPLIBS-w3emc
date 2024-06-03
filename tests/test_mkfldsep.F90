74c766e7a7f8fd6d49626016b0d48a09d63b54c1
Edward Hartnett <edward.hartnett@noaa.gov>
2024-06-03 05:11:59 -0600

    adding test for mkfldsep

---
+++ tests/test_mkfldsep.F90
@@ -0,0 +1,11 @@
+! This is a test in the NCEPLIBS-w3emc project.
+!
+! Test the mkfldsep subroutine. This is used in grip_utils utilities tocgrib2 and tocgrib2super.
+!
+! Edward Hartnett, 6/3/24
+program test_mkflsep
+  implicit none
+  print *, "Testing mkfldsep()..."
+  print *, "SUCCESS"
+end program test_mkflsep
+
