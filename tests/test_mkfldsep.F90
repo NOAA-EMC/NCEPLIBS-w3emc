e7a2d01b465ea3c0af0abc4d6e97bffacf6e3169
Edward Hartnett <edward.hartnett@noaa.gov>
2024-06-03 06:50:21 -0600

    more testing

--- tests/test_mkfldsep.F90
+++ tests/test_mkfldsep.F90
@@ -14,28 +14,15 @@ program test_mkflsep
   
   print *, "Testing mkfldsep(), expect and ignore error messages..."
 
-  print *, 'testing with iopt 1, lenin > 18...'
-  iopt = 1
-  lenin = 18
+  print *, 'testing iopt failure...'
+  iopt = 3
+  lenin = 19
   itot = 5000
   lenbull = itot + lenhead
   call mkfldsep(csep, iopt, lenin, lenbull, lenout)
   do i = 1, lenin
-     print *, i, ichar(csep(i))
+     if (csep(i) .ne. ' ') stop 10
   end do
-
-  ! Check every byte of result.
-  do i = 1, 4
-     if (csep(i) .ne. '#') stop 100
-  end do
-  if (csep(5) .ne. '0' .or. csep(6) .ne. '1' .or. csep(7) .ne. '8') stop 101
-  ! This is the length of message plus header as an i6.6.
-  if (csep(8) .ne. '0' .or. csep(9) .ne. '0' .or. csep(10) .ne. '5') stop 102
-  if (csep(11) .ne. '0' .or. csep(12) .ne. '2' .or. csep(13) .ne. '1') stop 103
-  do i = 14, 17
-     if (csep(i) .ne. '#') stop 105
-  end do
-  if (csep(18) .ne. char(10)) stop 107
   print *, 'OK!'
 
   print *, 'testing with iopt 1, lenin = 18...'
@@ -44,34 +31,74 @@ program test_mkflsep
   itot = 5000
   lenbull = itot + lenhead
   call mkfldsep(csep, iopt, lenin, lenbull, lenout)
-  do i = 1, lenin
-     print *, i, ichar(csep(i))
+  ! do i = 1, lenin
+  !    print *, i, ichar(csep(i))
+  ! end do
+
+  ! Check every byte of result.
+  do i = 1, 4
+     if (csep(i) .ne. '#') stop 200
   end do
+  if (csep(5) .ne. '0' .or. csep(6) .ne. '1' .or. csep(7) .ne. '8') stop 201
+  ! This is the length of message plus header as an i6.6.
+  if (csep(8) .ne. '0' .or. csep(9) .ne. '0' .or. csep(10) .ne. '5') stop 202
+  if (csep(11) .ne. '0' .or. csep(12) .ne. '2' .or. csep(13) .ne. '1') stop 203
+  do i = 14, 17
+     if (csep(i) .ne. '#') stop 205
+  end do
+  if (csep(18) .ne. char(10)) stop 207
+  print *, 'OK!'
+  
+  print *, 'testing with iopt 1, lenin = 23...'
+  iopt = 1
+  lenin = 23
+  itot = 5000
+  lenbull = itot + lenhead
+  call mkfldsep(csep, iopt, lenin, lenbull, lenout)
+  ! do i = 1, lenin
+  !    print *, i, ichar(csep(i))
+  ! end do
 
   ! Check every byte of result.
   do i = 1, 4
      if (csep(i) .ne. '#') stop 100
   end do
-  if (csep(5) .ne. '0' .or. csep(6) .ne. '1' .or. csep(7) .ne. '8') stop 101
+  if (csep(5) .ne. '0' .or. csep(6) .ne. '2' .or. csep(7) .ne. '3') stop 101
   ! This is the length of message plus header as an i6.6.
-  if (csep(8) .ne. '0' .or. csep(9) .ne. '0' .or. csep(10) .ne. '5') stop 102
-  if (csep(11) .ne. '0' .or. csep(12) .ne. '2' .or. csep(13) .ne. '1') stop 103
-  do i = 14, 17
-     if (csep(i) .ne. '#') stop 105
+  if (csep(8) .ne. '0' .or. csep(9) .ne. '0' .or. csep(10) .ne. '0') stop 102
+  if (csep(11) .ne. '0' .or. csep(12) .ne. '0' .or. csep(13) .ne. '0') stop 103
+  if (csep(14) .ne. '0' .or. csep(15) .ne. '5' .or. csep(16) .ne. '0') stop 104
+  if (csep(17) .ne. '2' .or. csep(18) .ne. '1') stop 105
+  do i = 19, lenin - 5 ! not used in this case
+     if (csep(i) .ne. '0') stop 110
   end do
-  if (csep(18) .ne. char(10)) stop 107
+  do i = lenin - 4, lenin - 1
+     if (csep(i) .ne. '#') stop 115
+  end do
+  if (csep(lenin) .ne. char(10)) stop 120
   print *, 'OK!'
   
-  ! ! This will fail, because iopt must be 1 or 2. csep will be set to
-  ! ! spaces.
-  ! iopt = 3
-  ! lenin = 19
-  ! itot = 5000
-  ! lenbull = itot + lenhead
-  ! call mkfldsep(csep, iopt, lenin, lenbull, lenout)
-  ! do i = 1, lenin
-  !    if (csep(i) .ne. ' ') stop 10
+  print *, 'testing with iopt 2...'
+  iopt = 2
+  lenin = 23
+  itot = 5000
+  lenbull = itot + lenhead
+  call mkfldsep(csep, iopt, lenin, lenbull, lenout)
+  ! do i = 1, lenout
+  !    print *, i, ichar(csep(i))
   ! end do
+
+  ! Check every byte of result.
+  do i = 1, 4
+     if (csep(i) .ne. '*') stop 300
+  end do
+  if (csep(5) .ne. '0' .or. csep(6) .ne. '0' .or. csep(7) .ne. '0') stop 302
+  if (csep(8) .ne. '0' .or. csep(9) .ne. '0' .or. csep(10) .ne. '0') stop 303
+  if (csep(11) .ne. '5' .or. csep(12) .ne. '0' .or. csep(13) .ne. '2') stop 304
+  if (csep(14) .ne. '1') stop 305
+  if (csep(15) .ne. '*' .or. csep(16) .ne. '*' .or. csep(17) .ne. '*') stop 320
+  if (csep(18) .ne. '*' .or. csep(19) .ne. char(10)) stop 305
+  print *, 'OK!'
   
   print *, "SUCCESS"
 end program test_mkflsep
