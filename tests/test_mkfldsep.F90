4e3431e562774d14b762f03a076ffcb58bccc8cf
Edward Hartnett <edward.hartnett@noaa.gov>
2024-06-04 05:03:28 -0600

    improved test of mkfldsep()

--- tests/test_mkfldsep.F90
+++ tests/test_mkfldsep.F90
@@ -8,7 +8,8 @@ program test_mkflsep
   
   character * 1   csep(80), lenbull_cin(4)
   integer :: iopt, lenin, lenbull, lenbull_in
-  integer :: lenout, itot, i
+  integer :: lenin_values(3) = (/ 21, 23, 25 /)
+  integer :: lenout, itot, i, j, nnn
   integer, parameter :: lenhead = 21
   character(len=4),parameter :: cstar='****', clb='####'
   
@@ -25,60 +26,46 @@ program test_mkflsep
   end do
   print *, 'OK!'
 
-  print *, 'testing with iopt 1, lenin = 18...'
-  iopt = 1
-  lenin = 18
-  itot = 5000
-  lenbull = itot + lenhead
-  call mkfldsep(csep, iopt, lenin, lenbull, lenout)
-  if (lenout .ne. 18) stop 199
-  ! do i = 1, lenin
-  !    print *, i, ichar(csep(i))
-  ! end do
+  ! Test iopt 1 with 3 different values for lenin.
+  do j = 1, 3
+     iopt = 1
+     lenin = lenin_values(j)
+     nnn = lenin
+     print *, 'testing with iopt 1, lenin = ', lenin
+     if (nnn .lt. 23) nnn = 23
+     itot = 5000
+     lenbull = itot + lenhead
+     call mkfldsep(csep, iopt, lenin, lenbull, lenout)
+     if (lenout .ne. nnn) stop 299
+     ! do i = 1, lenout
+     !    print *, i, ichar(csep(i))
+     ! end do
 
-  ! Check every byte of result.
-  do i = 1, 4
-     if (csep(i) .ne. '#') stop 200
+     ! Check every byte of result.
+     do i = 1, 4
+        if (csep(i) .ne. '#') stop 100
+     end do
+     if (csep(5) .ne. '0' .or. csep(6) .ne. '2') stop 101
+     if (j .le. 2) then
+        if (csep(7) .ne. '3') stop 102
+     else
+        if (csep(7) .ne. '5') stop 102
+     endif
+     ! This is the length of message plus header as an i6.6.
+     if (csep(8) .ne. '0' .or. csep(9) .ne. '0' .or. csep(10) .ne. '0') stop 105
+     if (csep(11) .ne. '0' .or. csep(12) .ne. '0' .or. csep(13) .ne. '0') stop 113
+     if (csep(14) .ne. '0' .or. csep(15) .ne. '5' .or. csep(16) .ne. '0') stop 114
+     if (csep(17) .ne. '2' .or. csep(18) .ne. '1') stop 115
+     ! I think this should be a range of 19:nnn-5.
+     if (nnn > 23) then
+        if (csep(19) .ne. '0') stop 130
+     endif
+     do i = nnn - 4, nnn - 1
+        if (csep(i) .ne. '#') stop 135
+     end do
+     if (csep(nnn) .ne. char(10)) stop 140
+     print *, 'OK!'
   end do
-  if (csep(5) .ne. '0' .or. csep(6) .ne. '1' .or. csep(7) .ne. '8') stop 201
-  ! This is the length of message plus header as an i6.6.
-  if (csep(8) .ne. '0' .or. csep(9) .ne. '0' .or. csep(10) .ne. '5') stop 202
-  if (csep(11) .ne. '0' .or. csep(12) .ne. '2' .or. csep(13) .ne. '1') stop 203
-  do i = 14, 17
-     if (csep(i) .ne. '#') stop 205
-  end do
-  if (csep(18) .ne. char(10)) stop 207
-  print *, 'OK!'
-  
-  print *, 'testing with iopt 1, lenin = 23...'
-  iopt = 1
-  lenin = 23
-  itot = 5000
-  lenbull = itot + lenhead
-  call mkfldsep(csep, iopt, lenin, lenbull, lenout)
-  if (lenout .ne. 23) stop 299
-  ! do i = 1, lenin
-  !    print *, i, ichar(csep(i))
-  ! end do
-
-  ! Check every byte of result.
-  do i = 1, 4
-     if (csep(i) .ne. '#') stop 100
-  end do
-  if (csep(5) .ne. '0' .or. csep(6) .ne. '2' .or. csep(7) .ne. '3') stop 101
-  ! This is the length of message plus header as an i6.6.
-  if (csep(8) .ne. '0' .or. csep(9) .ne. '0' .or. csep(10) .ne. '0') stop 102
-  if (csep(11) .ne. '0' .or. csep(12) .ne. '0' .or. csep(13) .ne. '0') stop 103
-  if (csep(14) .ne. '0' .or. csep(15) .ne. '5' .or. csep(16) .ne. '0') stop 104
-  if (csep(17) .ne. '2' .or. csep(18) .ne. '1') stop 105
-  do i = 19, lenin - 5 ! not used in this case
-     if (csep(i) .ne. '0') stop 110
-  end do
-  do i = lenin - 4, lenin - 1
-     if (csep(i) .ne. '#') stop 115
-  end do
-  if (csep(lenin) .ne. char(10)) stop 120
-  print *, 'OK!'
   
   print *, 'testing with iopt 2...'
   iopt = 2
